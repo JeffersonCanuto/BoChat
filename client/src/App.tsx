ac802361872bd6825a1ebee35d4469eb2e1cd011
jefferson <jeffersondscanuto@gmail.com>
2025-09-10 08:57:54 -0300

    chore: update .env.example with datadog variables

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import ChatPage from '@/pages/ChatPage';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
-
 const App:React.FC = () => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <ChatPage />
-        </QueryClientProvider>
-    );
+    return <ChatPage />;
 }
 
 export default App;
